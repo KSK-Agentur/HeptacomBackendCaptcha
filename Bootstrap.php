c0ba4eadfe10e517ffde2d84f86b39745b85b3a2
Julian Krzefski <krzefski@heptacom.de>
2016-11-03 13:54:43 +0100

    Added basic bootstrap file and plugin.json information file.

---
+++ Bootstrap.php
@@ -0,0 +1,105 @@
+<?php
+
+class Shopware_Plugins_Backend_HeptacomBackendCaptcha_Bootstrap extends Shopware_Components_Plugin_Bootstrap
+{
+    protected $pluginInfo;
+
+    protected function getPluginInfo($key = null, $default = null)
+    {
+        if (is_null($this->pluginInfo)) {
+            $this->pluginInfo = json_decode(file_get_contents(implode(DIRECTORY_SEPARATOR, [__DIR__, 'plugin.json'])), true);
+        }
+
+        if (!is_null($key)) {
+            if (array_key_exists($key, $this->pluginInfo)) {
+                return $this->pluginInfo[$key];
+            } else {
+                if (!is_null($default)) {
+                    return $default;
+                } else {
+                    throw new Enlight_Exception('Plugin information "' . $key . '" not found.');
+                }
+            }
+        } else {
+            return $this->pluginInfo;
+        }
+    }
+
+    public function getVersion()
+    {
+        return $this->getPluginInfo('currentVersion');
+    }
+
+    public function getLabel()
+    {
+        return $this->getPluginInfo('label');
+    }
+
+    public function getInfo()
+    {
+        return array(
+            'version' => $this->getVersion(),
+            'label' => $this->getLabel(),
+            'supplier' => $this->getPluginInfo('author'),
+            'author' => $this->getPluginInfo('author'),
+            'description' => $this->getPluginInfo('description', ''),
+            'link' => $this->getPluginInfo('link'),
+        );
+    }
+
+    public function install()
+    {
+        $compatibility = $this->getPluginInfo('compatibility');
+        if (!$this->assertMinimumVersion($compatibility['minimumVersion'])) {
+            throw new Enlight_Exception('At least Shopware ' . $compatibility['minimumVersion'] . ' is required');
+        }
+
+        $this->createConfiguration();
+
+        $this->subscribeEvent(
+            'Enlight_Controller_Front_DispatchLoopStartup',
+            'onStartDispatch'
+        );
+
+        return true;
+    }
+
+    public function enable()
+    {
+        return [
+            'success' => true,
+            'invalidateCache' => ['template']
+        ];
+    }
+
+    public function disable()
+    {
+        return [
+            'success' => true,
+            'invalidateCache' => ['template']
+        ];
+    }
+
+    public function uninstall()
+    {
+        return $this->disable();
+    }
+
+    public function update($oldVersion)
+    {
+        return true;
+    }
+
+    public function onStartDispatch(Enlight_Event_EventArgs $args)
+    {
+        $this->registerMyComponents();
+    }
+
+    public function registerMyComponents()
+    {
+        $this->Application()->Loader()->registerNamespace(
+            'Shopware\HeptacomBackendCaptcha',
+            $this->Path()
+        );
+    }
+}
