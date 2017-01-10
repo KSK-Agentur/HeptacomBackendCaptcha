0d2be388aac4cf8ebd9e8cf458531987616776c5
Julian Krzefski <krzefski@heptacom.de>
2017-01-10 11:24:42 +0100

    Got invisible mode working. Also some minor style adjustments.

--- Resources/views/backend/plugins/heptacom_backend_captcha/header.tpl
+++ Resources/views/backend/plugins/heptacom_backend_captcha/header.tpl
@@ -1,5 +1,9 @@
 {block name="backend/base/header/css" append}
-    <link rel="stylesheet" type="text/css" href="{link file='backend/plugins/heptacom_backend_captcha/resources/css/login_window.css'}?{Shopware::REVISION}" />
+    {if $heptacomBackendCaptcha.visibility}
+        <link rel="stylesheet" type="text/css" href="{link file='backend/plugins/heptacom_backend_captcha/resources/css/visible.css'}?{Shopware::REVISION}" />
+    {else}
+        <link rel="stylesheet" type="text/css" href="{link file='backend/plugins/heptacom_backend_captcha/resources/css/invisible.css'}?{Shopware::REVISION}" />
+    {/if}
 {/block}
 
 {block name="backend/base/header/javascript" append}
--- Resources/views/backend/plugins/heptacom_backend_captcha/view/main/invisible.js
+++ Resources/views/backend/plugins/heptacom_backend_captcha/view/main/invisible.js
@@ -1,38 +1,60 @@
 // invisible captcha
-function YourOnSubmitFn(token) {
-    console.log('test');
-}
+var reCaptchaOnSubmit = function(token) {
+    var btn = Ext.getCmp(loginButtonId);
+    var win = btn.up('window');
+    var formPnl = win.down('form');
+
+    var captchaField = Ext.create('Ext.form.field.Hidden');
+    captchaField.name = 'g-recaptcha-response';
+    captchaField.setValue(token);
+    formPnl.add(captchaField);
+
+    var form = formPnl.getForm();
+    var values = form.getValues();
+
+    if(!form.isValid() || !values.password.length || !values.username.length) {
+        return false;
+    }
+    form.submit({
+        url: '{url action=login}',
+        waitMsg: '{s name=wait/message}Login...{/s}',
+        success: function(form, action) {
+            window.location.href = window.location.href;
+        },
+        failure: function(form, action) {
+            var lockedUntil, message;
+            if(action.result.lockedUntil) {
+                action.result.lockedUntil = new Date(action.result.lockedUntil);
+                message = "{s name=failure/locked_message}Der Account ist bis zum [lockedUntil:date] um [lockedUntil:date('H:i:s')] Uhr gesperrt.{/s}";
+                message = new Ext.Template(message);
+                message = message.applyTemplate(action.result);
+            } else {
+                message = '{s name=failure/input_message}Bitte überprüfen Sie Ihre Eingabe und probieren es erneut.{/s}';
+            }
+            Ext.Msg.alert('{s name=failure/title}Login fehlgeschlagen{/s}', '{s name=failure/message}Ihr Login war nicht erfolgreich. {/s}' + message);
+            return false;
+        }
+    });
+};
 
 Ext.define('Shopware.apps.HeptacomBackendCaptcha.view.main.Form', {
     override:'Shopware.apps.Login.view.main.Form',
 
     listeners: {
-        render: function() {
-            window.grecaptcha.render('heptacom_backend_captcha', {
-                'sitekey': '{$heptacomBackendCaptcha.sitekey}'
+        afterrender: function() {
+            window.loginButtonId = this.dockedItems.items[0].items.items[1].id;
+
+            grecaptcha.render(loginButtonId + '-btnEl', {
+                'sitekey': '{$heptacomBackendCaptcha.sitekey}',
+                'callback' : reCaptchaOnSubmit
             });
         },
         beforeaction: function(subject, action) {
-            action.params['g-recaptcha-response'] = Ext.getElementById('g-recaptcha-response').value;
-        }
-    },
+            var formValues = action.form.getFieldValues();
 
-    initComponent: function() {
-        var me = this;
-
-        me.callParent(arguments);
-
-        if(Ext.ieVersion === 0 || Ext.ieVersion >= 9) {
-            me.captcha = Ext.create('Ext.container.Container', {
-                html: '<button class="g-recaptcha" data-sitekey="{$heptacomBackendCaptcha.sitekey}" data-callback="YourOnSubmitFn">Submit</button>'
-            });
-
-            me.add(me.captcha);
+            if (!formValues['g-recaptcha-response']) {
+                return false;
+            }
         }
     }
-});
-
-Ext.define('Shopware.apps.HeptacomBackendCaptcha.view.Main', {
-    override: 'Shopware.apps.Login.view.Main',
-    height: 480
 });
